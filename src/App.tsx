23aa359d7bc035ae25295f9395885990b137761d
Anant Narayan <anantnrg@gmail.com>
2025-07-28 23:08:39 +0530

    back to 'class' and fuck it, just hardcoded colors for now

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,26 @@ import connectedIcon from "./assets/icons/connected.svg";
 
 function App() {
   return (
-    <main className="bg-bg h-screen w-screen flex flex-col p-3 items-center justify-center gap-y-3">
-      <div className="w-full h-12 bg-panels border border-border flex items-center justify-between px-4">
-        <span className="text-2xl text-heading uppercase font-semibold tracking-wider">
+    <main class="bg-bg h-screen w-screen flex flex-col p-3 items-center justify-center gap-y-3">
+      <div class="w-full h-12 bg-panels border border-border flex items-center justify-between px-4">
+        <span class="text-2xl text-heading uppercase font-semibold tracking-wider">
           NaughtyBug Core-x
         </span>
-        <span className="text-lg text-heading uppercase flex gap-x-2 items-center justify-center">
-          <img
-            src={connectedIcon}
-            className="w-7 text-heading"
-            alt="connected"
-          />
-          Connected
+        <span class="text-lg text-heading uppercase flex gap-x-1 items-center justify-center">
+          <img src={connectedIcon} class="w-5" /> Connected
         </span>
       </div>
-      <div className="w-full h-full flex gap-x-3">
-        <div className="w-2/3 h-full flex flex-col gap-y-3">
-          <div className="w-full h-1/2 flex gap-x-3">
-            <div className="w-1/2 h-full bg-panels border border-border"></div>
-            <div className="w-1/2 h-full bg-panels border border-border"></div>
+      <div class="w-full h-full flex gap-x-3">
+        <div class="w-2/3 h-full flex flex-col gap-y-3">
+          <div class="w-full h-1/2 flex gap-x-3">
+            <div class="w-1/2 h-full bg-panels border border-border"></div>
+            <div class="w-1/2 h-full bg-panels border border-border"></div>
           </div>
-          <div className="w-full h-1/2 flex gap-x-3 bg-panels border border-border"></div>
+          <div class="w-full h-1/2 flex gap-x-3 bg-panels border border-border"></div>
         </div>
-        <div className="w-1/3 h-full flex flex-col gap-y-3">
-          <div className="w-full h-1/2 bg-panels border border-border"></div>
-          <div className="w-full h-1/2 bg-panels border border-border"></div>
+        <div class="w-1/3 h-full flex flex-col gap-y-3">
+          <div class="w-full h-1/2 bg-panels border border-border"></div>
+          <div class="w-full h-1/2 bg-panels border border-border"></div>
         </div>
       </div>
     </main>
