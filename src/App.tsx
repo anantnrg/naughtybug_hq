e85754eb7d5039daacc94b5b32e27caf74aaee66
Anant Narayan <anantnrg@gmail.com>
2025-07-28 20:52:12 +0530

    cool shit

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <main class="bg-bg h-screen w-screen">
-      <h1>NaughtyBug HQ</h1>
+    <main class="bg-bg h-screen w-screen flex flex-col p-3 items-center justify-center">
+      <div class="w-full h-12 bg-panels border border-border flex items-center justify-between px-4">
+        <span class="text-2xl text-heading uppercase font-semibold tracking-wider">
+          NaughtyBug Core-x
+        </span>
+        <span class="text-lg text-heading uppercase">Connected</span>
+      </div>
     </main>
   );
 }
